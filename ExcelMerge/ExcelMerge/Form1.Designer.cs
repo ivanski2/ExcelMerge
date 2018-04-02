9d2bc2b8a336bb4a45209e873b7025c7c7db12ab
Иван Иванов <ivanski34@gmail.com>
2018-04-02 11:26:47 +0300

    Add files via upload

---
+++ ExcelMerge/ExcelMerge/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ExcelMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Merge = new System.Windows.Forms.Button();
+            this.Select_all = new System.Windows.Forms.Button();
+            this.Browse = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Merge
+            // 
+            this.Merge.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Merge.Location = new System.Drawing.Point(101, 395);
+            this.Merge.Name = "Merge";
+            this.Merge.Size = new System.Drawing.Size(75, 23);
+            this.Merge.TabIndex = 1;
+            this.Merge.Text = "Merge";
+            this.Merge.UseVisualStyleBackColor = true;
+            this.Merge.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Select_all
+            // 
+            this.Select_all.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Select_all.Location = new System.Drawing.Point(20, 395);
+            this.Select_all.Name = "Select_all";
+            this.Select_all.Size = new System.Drawing.Size(75, 23);
+            this.Select_all.TabIndex = 4;
+            this.Select_all.Text = "Select all";
+            this.Select_all.UseVisualStyleBackColor = true;
+            this.Select_all.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Browse.Location = new System.Drawing.Point(39, 12);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 11;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(12, 44);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(245, 329);
+            this.listbox.TabIndex = 0;
+            this.listbox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Clear.Location = new System.Drawing.Point(182, 395);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 13;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Delete.Location = new System.Drawing.Point(138, 12);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 14;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(283, 430);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.Select_all);
+            this.Controls.Add(this.Merge);
+            this.Controls.Add(this.listbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Merge;
+        private System.Windows.Forms.Button Select_all;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.ListBox listbox;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ ExcelMerge/ExcelMerge/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Spire.Xls;
+
+
+
+
+namespace ExcelMerge
+{
+    public partial class Form1 : Form
+    {
+        string[] filename;
+        string[] arrAllFiles=new string[]{};
+        int count = Properties.Settings.Default.count;
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listbox.SelectionMode = SelectionMode.MultiSimple;
+            for (int i = 0; i < listbox.Items.Count; i++)
+                listbox.SetSelected(i, true);
+           
+            //add_listbox.SelectionMode = //back to what you want
+        }
+
+       
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       private void button10_Click(object sender, EventArgs e)
+        {
+            while (listbox.SelectedItems.Count > 0)
+            {
+                listbox.Items.Remove(listbox.SelectedItems[0]);
+            }
+
+        }
+       
+        private void button8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.xls*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+            string path = Directory.GetCurrentDirectory();
+            //choofdlog.InitialDirectory = path; //
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                arrAllFiles = choofdlog.FileNames;
+                filename = choofdlog.SafeFileNames;
+                for (int i = 0; i < arrAllFiles.Length;i++ )
+                {
+                    listbox.Items.Add(filename[i]);
+                }
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.count++;
+            Properties.Settings.Default.Save();
+            Workbook newbook = new Workbook();
+            newbook.Version = ExcelVersion.Version2010;
+            newbook.Worksheets.Clear();
+
+            Workbook tempbook = new Workbook();
+            List<string> excelFiles = new List<string>();
+            for (var i = 0; i < listbox.SelectedItems.Count; i++)
+            {
+                excelFiles.Add(filename[i]);
+
+            }
+            if (listbox.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Not selected item");
+            }
+            else
+            {
+                for (int i = 0; i < excelFiles.Count; i++)
+                {
+                    tempbook.LoadFromFile(excelFiles[i]);
+                    foreach (Worksheet sheet in tempbook.Worksheets)
+                    {
+
+                        newbook.Worksheets.AddCopy(sheet);
+                    }
+
+                }
+
+                newbook.SaveToFile(count + "result1.xlsx", ExcelVersion.Version2010);
+                System.Diagnostics.Process.Start(count + "result1.xlsx");
+            }
+        }
+            
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            for (var i = 0; i < arrAllFiles.Length; i++)
+            {
+                File.Delete(arrAllFiles[i]);
+            }
+            
+            MessageBox.Show("Files deleted");
+            listbox.Items.Clear();
+        }
+
+
+    }
+}
